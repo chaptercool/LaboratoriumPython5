110b921de3676a838b2e96f5544e17a4ecddf96d
w68141 <yr.hvrn15@gmail.com>
2022-11-17 13:07:59 +0100

    pop

--- 2.py
+++ 2.py
@@ -8,11 +8,20 @@ sample_dict = {
 for k, v in sample_dict.items():
     print(f'{k:<8} = {v:>8}')
 
-list = ["name", "age"]
 D = {}
 
-for k in list:
+L = ["name", "age"]
+for k in L:
     if k in sample_dict.keys():
         D[k] = sample_dict[k]
 
 print(D)
+
+L = ["name", "age"]
+# for k in L:
+#     if k in sample_dict.keys:
+#         sample_dict.pop(k)
+
+for k in L:
+    sample_dict.pop(k, None)
+print(sample_dict)
